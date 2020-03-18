daf46d6ee6e9f54e8a87ca5011f33e1ff16c184b
resourceful-role-9993 <unicetee@gmail.com>
2020-03-18 23:23:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,9 @@
 def square_array(array)
   # your code here
-   array.each  do|k|;
 newarr=[];
-      newarr << k**2
-       return newarr
-
+   array.each  do|k|;
+  newarr << k**2
    end
+   return newarr
 end
 square_array array=[1,2,3]
